2c2bfc505457336a975a756bd63a81719186a6a2
Michael Mugnai <michael.mugnai@gmail.com>
2020-10-15 13:50:46 +0200

    hybrid graphics and multi-monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,8 +11,25 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bars
 polybar -c ~/.config/polybar/config.ini main &
 
-is_external_monitor_present=$(xrandr --query | grep '^DP1 connected')
-if [[ $is_external_monitor_present ]]; then
-    polybar -c ~/.config/polybar/config_second_monitor.ini main &
+# HDMI port
+if [[ $(xrandr --query | grep '^HDMI-1-0 connected') ]]; then
+    polybar -c ~/.config/polybar/config_hdmi-1-0.ini main &
 fi
 
+# MiniDP port (daisy chain)
+if [[ $(xrandr --query | grep '^DP-1-0.8 connected') ]]; then
+    polybar -c ~/.config/polybar/config_dp-1-0.8.ini main &
+fi
+
+if [[ $(xrandr --query | grep '^DP-1-0.1.8 connected') ]]; then
+    polybar -c ~/.config/polybar/config_dp-1-0.1.8.ini main &
+fi
+
+# type-C port (daisy chain)
+if [[ $(xrandr --query | grep '^DP-1-8 connected') ]]; then
+    polybar -c ~/.config/polybar/config_usb_c-1-8.ini main &
+fi
+
+if [[ $(xrandr --query | grep '^DP-1-1-8 connected') ]]; then
+    polybar -c ~/.config/polybar/config_usb_c-1-1-8.ini main &
+fi
