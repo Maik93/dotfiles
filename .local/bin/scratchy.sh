c785513f6d1a6a330f56dc843234ac08fadc4cbd
Michael Mugnai <michael.mugnai@gmail.com>
2021-03-10 11:56:09 +0100

    scratchy windows with different shapes

--- .local/bin/scratchy.sh
+++ .local/bin/scratchy.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-id=$(xdo id -n scratchy);
-if [ -z "$id" ]; then
-  urxvt -name scratchy;
+echo "$1";
+if [ -z "$1" ]; then
+	size=80x16;
 else
-  action='hide';
-  if [[ $(xprop -id $id | awk '/window state: / {print $3}') == 'Withdrawn' ]]; then
-    action='show';
-  fi
-  xdo $action -n scratchy
+	# size=120x40;
+	size=$1;
 fi
+
+# id=$(xdo id -n scratchy);
+# if [ -z "$id" ]; then
+#   urxvt -name scratchy;
+# else
+#   action='hide';
+#   if [[ $(xprop -id $id | awk '/window state: / {print $3}') == 'Withdrawn' ]]; then
+#     action='show';
+#   fi
+#   xdo $action -n scratchy
+# fi
+
+urxvt -name scratchy -g "$size";
