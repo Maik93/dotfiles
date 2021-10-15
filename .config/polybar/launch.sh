99fbad1c407e75e9f7903654c5d272048d81f305
Michael Mugnai <michael.mugnai@gmail.com>
2021-10-15 15:34:18 +0200

    update multi-monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -14,22 +14,22 @@ polybar -c ~/.config/polybar/config.ini main &
 # HDMI port
 if [[ $(xrandr --query | grep '^HDMI-1-0 connected') ]]; then
     polybar -c ~/.config/polybar/config_hdmi-1-0.ini main &
-fi
+
+# type-C port (HDMI adaptor)
+elif [[ $(xrandr --query | grep '^DP-1 connected') ]]; then
+    polybar -c ~/.config/polybar/config_usb_c-1.ini main &
 
 # MiniDP port (daisy chain)
-if [[ $(xrandr --query | grep '^DP-1-0.8 connected') ]]; then
+elif [[ $(xrandr --query | grep '^DP-1-0.8 connected') ]]; then
     polybar -c ~/.config/polybar/config_dp-1-0.8.ini main &
-fi
 
-if [[ $(xrandr --query | grep '^DP-1-0.1.8 connected') ]]; then
+elif [[ $(xrandr --query | grep '^DP-1-0.1.8 connected') ]]; then
     polybar -c ~/.config/polybar/config_dp-1-0.1.8.ini main &
-fi
 
 # type-C port (daisy chain)
-if [[ $(xrandr --query | grep '^DP-1-8 connected') ]]; then
+elif [[ $(xrandr --query | grep '^DP-1-8 connected') ]]; then
     polybar -c ~/.config/polybar/config_usb_c-1-8.ini main &
 fi
-
 if [[ $(xrandr --query | grep '^DP-1-1-8 connected') ]]; then
     polybar -c ~/.config/polybar/config_usb_c-1-1-8.ini main &
 fi
